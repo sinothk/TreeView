929767a40dbc6129cced8b056c4c6d1a93711297
LiangYT <381518188@qq.com>
2019-09-23 15:58:22 +0800

    初始化！

--- app/src/main/java/com/sinothk/treeView/demo/view/RecyclerViewActivity.java
+++ app/src/main/java/com/sinothk/treeView/demo/view/RecyclerViewActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
