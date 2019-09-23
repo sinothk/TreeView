6a82b0466cc349c1edcd39090d39b8ff73432808
LiangYT <381518188@qq.com>
2019-09-23 14:19:46 +0800

    初始化！

---
+++ TreeView/src/main/java/com/sinothk/treeView/OnTreeNodeClickListener.java
@@ -0,0 +1,8 @@
+package com.sinothk.treeView;
+
+/**
+ * Created by zhangke on 2017-1-14.
+ */
+public interface OnTreeNodeClickListener {
+    void onClick(Node node, int position);
+}
---
+++ app/src/main/java/com/sinothk/treeView/demo/FileNode.java
@@ -0,0 +1,11 @@
+package com.sinothk.treeView.demo;
+
+/**
+ * Created by zhangke on 2017-1-14.
+ */
+public class FileNode  {
+
+
+    public String name = "666";
+
+}
